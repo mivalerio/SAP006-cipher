d3f1a758abcfd22e7c8470742aa84e2d1ecb3ced
Michele Martins <michele.valerio@outlook.com.br>
2021-06-11 17:38:57 -0300

    implementação da função encode/decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,52 @@
 const cipher = {
-  // ...
-};
+
+  encode: function criptografar(offset, string){
+
+  let textoCodificado = "";
+
+   for(let i = 0; i < string.length; i++) {
+     //variavel para guardar a letra codificada
+     let letraCodificada = string.charCodeAt(i);
+      //loop para pegar e salvar o numero na tab. ASC
+      if(letraCodificada >= 65 && letraCodificada <=90){
+        //variavel recebe a letra ja transformada com o valor do deslocamento
+         //formula do Daniel
+        letraCodificada = ((letraCodificada - 65 + offset) % 26 +65);
+
+      }
+     //transforma o codigo de numero da tab Asc para letra e joga na variavel texto
+     textoCodificado += String.fromCharCode(letraCodificada);
+     
+    }
+    return textoCodificado;
+
+    //console.log("funcionou");
+  },
+
+  
+
+  decode: function descriptografar(offset, string){
+
+    let textoCodificado = "";
+  
+     for(let i = 0; i < string.length; i++) {
+       //variavel para guardar a letra codificada
+       let letraCodificada = string.charCodeAt(i);
+        //loop para pegar e salvar o numero na tab. ASC
+        if(letraCodificada >= 65 && letraCodificada <=90){
+          //variavel recebe a letra ja transformada com o valor do deslocamento
+           //formula do Daniel
+          letraCodificada = ((letraCodificada + 65 - offset) % 26 +65);
+  
+        }
+       //transforma o codigo de numero da tab Asc para letra e joga na variavel texto
+       textoCodificado += String.fromCharCode(letraCodificada);
+       
+      }
+      return textoCodificado;
+  
+      //console.log("funcionou");
+    }
+  }
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
+//const texto = document.getElementById("mensagem");
+//const number = document.getElementById("offset");
 
-console.log(cipher);
+console.log(cipher.encode(3, "MICHELE VALERIO MARTINS"));
+console.log(cipher.decode(3, "PLFKHOH YDOHULR PDUWLQV"));
+
+
+
